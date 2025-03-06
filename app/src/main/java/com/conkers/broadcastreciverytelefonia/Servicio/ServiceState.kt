a00a379186cd568ac8e9d1a8375a147f7dfc07bf
Conkers1800 <diaz.1800xd@gmail.com>
2025-03-06 16:16:01 -0600

    Actualizacion del manifiesto para algunos permisos de telefonia como call log. Tambien se hizo un ajuste en el ServiceState

--- app/src/main/java/com/conkers/broadcastreciverytelefonia/Servicio/ServiceState.kt
+++ app/src/main/java/com/conkers/broadcastreciverytelefonia/Servicio/ServiceState.kt
@@ -8,23 +8,18 @@ import android.telephony.TelephonyManager
 import com.conkers.broadcastreciverytelefonia.componentes.MyBroadcastReceiver
 
 class ServiceState : Service() {
-    val br: MyBroadcastReceiver = MyBroadcastReceiver()
-    val intentFilter: IntentFilter = IntentFilter().apply {
+    private val br = MyBroadcastReceiver()
+    private val intentFilter = IntentFilter().apply {
         addAction(TelephonyManager.ACTION_PHONE_STATE_CHANGED)
-        addAction(Intent.ACTION_BOOT_COMPLETED)
     }
 
-    override fun onBind(p0: Intent?): IBinder? {
+    override fun onBind(intent: Intent?): IBinder? {
         return null
     }
 
     override fun onCreate() {
         super.onCreate()
         registerReceiver(br, intentFilter)
-    }
-
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        return super.onStartCommand(intent, flags, startId)
     }
 
     override fun onDestroy() {
